ad258cc44658e0fe550436ffe553525dec92e39d
Marc Cenac <marc@codeforneworleans.org>
2017-09-23 21:49:26 -0500

    use legitimate user-agent string

--- scraper/settings.py
+++ scraper/settings.py
@@ -15,7 +15,7 @@ SPIDER_MODULES = ['scraper.spiders']
 NEWSPIDER_MODULE = 'scraper.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+USER_AGENT = 'codefornolabot/1.0'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
