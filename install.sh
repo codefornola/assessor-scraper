1e42ea28da51e9b0be7ed6411722887206b06ce0
dstuck <david.e.stuck@gmail.com>
2019-04-11 20:49:37 -0500

    Add script for installing package

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt-get -y update
+sudo apt-get -y install build-essential python3-dev
+sudo apt-get -y install python-pip
+
+sudo apt-get -y install postgresql postgresql-contrib libpq-dev
+sudo service postgresql start
+
+sudo -u postgres createuser -s -d assessor
+sudo -u postgres psql -c "ALTER USER assessor WITH PASSWORD 'assessor';"
+sudo -u postgres createdb assessor
+sudo -u postgres psql -c "grant all privileges on database assessor to assessor"
+
+sudo pip install virtualenv
+
+git clone https://github.com/dstuck/assessor-scraper.git
+
+virtualenv -p python3 venv_scraper
+
+. venv_scraper/bin/activate
+
+pip install requests
+pip install psycopg2==2.7.3.2
+pip install pyproj
+pip install SQLAlchemy==1.1.15
+pip install Scrapy==1.4.0
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='assessor-scraper',
+    version='0.1',
+    description='',
+    url='https://github.com/codefornola/assessor-scraper',
+    author='CodeForNola',
+    author_email='',
+    packages=['scraper'],
+    install_requires=[
+        "psycopg2==2.7.3.2",
+        "pyproj",
+        "requests",
+        "Scrapy==1.4.0",
+        "SQLAlchemy==1.1.15",
+    ],
+    zip_safe=False
+)
